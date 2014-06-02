036024034071303156e5384f166d139661d4333d
Nate Murray <nate@natemurray.com>
2014-06-02 16:24:18 -0700

    initial import

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,14 +9,54 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var bird: SKSpriteNode?
+    var skyColor: SKColor?
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        // setup background color
+        skyColor = SKColor(red: 113.0/255.0, green: 197.0/255.0, blue: 207.0/255.0, alpha: 1.0)
+        self.backgroundColor = skyColor
         
-        self.addChild(myLabel)
+        // ground
+        var groundTexture = SKTexture(imageNamed: "land")
+        groundTexture.filteringMode = SKTextureFilteringMode.Nearest
+        
+        for var i = 0; CGFloat(i) < (2.0 + self.frame.size.width) / ( groundTexture.size().width * 2.0 ); ++i {
+            var sprite = SKSpriteNode(texture: groundTexture)
+            sprite.setScale(2.0)
+            sprite.position = CGPointMake(CGFloat(i) * sprite.size.width, sprite.size.height / 2.0)
+            self.addChild(sprite)
+        }
+        
+        // skyline
+        var skyTexture = SKTexture(imageNamed: "sky")
+        skyTexture.filteringMode = SKTextureFilteringMode.Nearest
+        
+        for var i = 0; CGFloat(i) < (2.0 + self.frame.size.width) / ( skyTexture.size().width * 2.0 ); ++i {
+            var sprite = SKSpriteNode(texture: skyTexture)
+            sprite.setScale(2.0)
+            sprite.zPosition = -20;
+            sprite.position = CGPointMake(CGFloat(i) * sprite.size.width, sprite.size.height / 2.0 + groundTexture.size().height * 2.0)
+            self.addChild(sprite)
+        }
+        
+        // setup our bird
+        var birdTexture1 = SKTexture(imageNamed: "bird-01")
+        birdTexture1.filteringMode = SKTextureFilteringMode.Nearest
+        var birdTexture2 = SKTexture(imageNamed: "bird-02")
+        birdTexture2.filteringMode = SKTextureFilteringMode.Nearest
+
+        var anim = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.2)
+        var flap = SKAction.repeatActionForever(anim)
+        
+        bird = SKSpriteNode(texture: birdTexture1)
+        bird!.setScale(2.0)
+        bird!.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        bird!.runAction(flap)
+        self.addChild(bird)
+        
+        
+
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
