677c42a6b47cccfc342c75069fff9733559e8e1a
Will Hodges <hodgeswt@users.noreply.github.com>
2015-10-19 16:04:14 -0400

    Update AppDelegate.swift
    Added line to hide status bar

--- FlappyBird/AppDelegate.swift
+++ FlappyBird/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        application.statusBarHidden = true
         return true
     }
 
