cb61163f16bc84d03a9a8ad1703415d2ccb94f73
Will Hodges <hodgeswt@users.noreply.github.com>
2015-10-19 16:05:26 -0400

    Update GameScene.swift
    Changed variable definitions from var to let or  _ when needed.

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -122,7 +122,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         self.addChild(bird)
         
         // create the ground
-        var ground = SKNode()
+        let ground = SKNode()
         ground.position = CGPoint(x: 0, y: groundTexture.size().height)
         ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.frame.size.width, height: groundTexture.size().height * 2.0))
         ground.physicsBody?.dynamic = false
@@ -168,7 +168,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         pipeUp.physicsBody?.contactTestBitMask = birdCategory
         pipePair.addChild(pipeUp)
         
-        var contactNode = SKNode()
+        let contactNode = SKNode()
         contactNode.position = CGPoint( x: pipeDown.size.width + bird.size.width / 2, y: self.frame.midY )
         contactNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize( width: pipeUp.size.width, height: self.frame.size.height ))
         contactNode.physicsBody?.dynamic = false
@@ -206,7 +206,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         /* Called when a touch begins */
         if moving.speed > 0  {
             for touch: AnyObject in touches {
-                let location = touch.locationInNode(self)
+                _ = touch.locationInNode(self)
                 
                 bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                 bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 30))
