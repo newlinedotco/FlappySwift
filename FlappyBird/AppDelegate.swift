93b1f9ccc831b2092f5978e9df518ec39f3085cf
Simina Pasat <siminap@microsoft.com>
2016-08-31 18:00:32 -0700

    comment 3 added

--- FlappyBird/AppDelegate.swift
+++ FlappyBird/AppDelegate.swift
@@ -37,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        //and another commit
     }
 
     func applicationWillTerminate(application: UIApplication) {
